967afce89e2cd693f3ff60d260de25c31555e6a1
chandan99 <chandanirmal99>
2026-05-14 01:29:18 +0530

    first commit

---
+++ BaseTest.java
@@ -0,0 +1,116 @@
+package rahulshettyacademy.TestComponents;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions; 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import rahulshettyacademy06.LandingPage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+public class BaseTest {
+
+	public WebDriver driver; // (This is public-can be used in all classes)
+	public LandingPage landingPage;
+
+	public WebDriver initializeDriver() throws IOException {
+
+		Properties prop = new Properties();
+
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+				+ "\\src\\main\\java\\rahulshettyacademy\\resources\\GlobalData.properties");
+
+		prop.load(fis);
+
+		String browserName = System.getProperty("browser") !=null ? System.getProperty("browser") : prop.getProperty("browser");
+		if (browserName.contains("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			WebDriverManager.chromedriver().setup();
+			if (browserName.contains("headless")) {
+				options.addArguments("headless");
+			}
+			driver = new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(1440,900));
+		}
+
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("Webdriver.gecko.driver","/Users/rahulshetty//documents//geckodriver");
+			driver = new FirefoxDriver();
+		}
+
+		else if (browserName.equalsIgnoreCase("edge")) {
+			// edge
+		}
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		return driver;
+
+	}
+
+	public List<HashMap<String, String>> getJasonDataToMap(String filePath) throws IOException {
+
+		// Read jason to String
+
+		String jsonContent = FileUtils.readFileToString(new File(filePath),
+				StandardCharsets.UTF_8);
+
+		// String to HashMap -Jackson Databind Dependency
+		//(
+			//	System.getProperty("user.dir") + "src//test//java//rahulshettyacademy//data//PurchaseOrder.json")
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
+		return data;
+
+	}
+
+	@BeforeMethod(alwaysRun = true)
+	public LandingPage launchAppication() throws IOException {
+
+		initializeDriver();
+		landingPage = new LandingPage(driver);
+		landingPage.goTo();
+		return landingPage;
+
+	}
+	
+	public String getScreenshot (String testCaseName, WebDriver driver) throws IOException {
+		
+		TakesScreenshot ts =(TakesScreenshot)driver;
+		File source =ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir")+ "//reports//"+testCaseName+".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir")+ "//reports//"+testCaseName+".png";
+		
+		
+		}
+
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() {
+		driver.close();
+	}
+
+}
