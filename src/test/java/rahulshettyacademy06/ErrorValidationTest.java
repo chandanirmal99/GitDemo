45e57b1a882fca8ced7c27bddf96c26f82a894b0
chandan99 <chandanirmal99>
2026-05-18 00:57:35 +0530

    Architect Asian development

--- src/test/java/rahulshettyacademy06/ErrorValidationTest.java
+++ src/test/java/rahulshettyacademy06/ErrorValidationTest.java
@@ -34,6 +34,13 @@ public class ErrorValidationTest extends BaseTest {
 		Boolean match = cartPage.verifyProductDisplay("ZARA COAT 33"); // // (Since here it return boolean and variable is required in next validation)
 		Assert.assertFalse(match);
 	}
+	
+	@Test
+	
+	public void Demo() {
+		System.out.println("I am fine");
+		System.out.println("I am happy");
+	}
 
 	
 	
