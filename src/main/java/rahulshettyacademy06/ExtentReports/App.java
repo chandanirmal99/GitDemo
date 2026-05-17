f310d4e1a46b49aa5fd1407e5f7c9776baea1ae1
chandan99 <chandanirmal99>
2026-05-18 01:28:49 +0530

    Extent Report

---
+++ src/main/java/rahulshettyacademy06/ExtentReports/App.java
@@ -0,0 +1,10 @@
+package rahulshettyacademy06.ExtentReports;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
